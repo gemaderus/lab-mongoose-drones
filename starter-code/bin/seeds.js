63e4e37422331ed58b83b4b8970274f268c9a73b
gemaderus <hi@gemaderus.com>
2017-11-09 15:07:02 +0100

    Second iteration finished.

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,39 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev', {useMongoClient: true});
+
+const Dron = require('../models/drone');
+
+// bin/seeds.js
+
+const drone = [
+ {
+   droneName  : "Manu",
+   propellers : 4,
+   maxSpeed   : 100,
+ },
+
+ {
+   droneName  : "Gema",
+   propellers : 2,
+   maxSpeed   : 120,
+ },
+];
+
+Dron.collection.drop(); //Elimina la colección asociada al modelo. Para que cada vez que lo ejecute, no lo vuelva a crear.
+
+
+Dron.create(drone, (err, docs) => {
+  //le pasamos como primer parámetro un array de objetos
+  if (err) {
+    throw err;
+  }
+
+  docs.forEach((dron) => {
+    console.log(dron.droneName)
+  });
+
+  //importante cerrar la conexión con mongoose
+  mongoose.connection.close();
+});
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,14 +1,22 @@
 const express = require('express');
 
 // require the Drone model here
+const Dron = require('../models/drone');
 
 const router = express.Router();
 
-
 router.get('/drones', (req, res, next) => {
-  // Iteration #2
-});
+  Dron.find({}, (err, drons) => {
+    if (err) {
+      return next(err) //Si hay un error se lo paso al siguiente y mientras me muestra un error que viene definido en app.js y nunca se mostraría el render de index. Es un middleware que ponemos
+    }
 
+    res.render('drones/index', {
+      //foo es el nombre de la colección
+      foo: drons
+    });
+  });
+});
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
